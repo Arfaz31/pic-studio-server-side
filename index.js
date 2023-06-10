1d58ea35f65ccebb379a5d679974bf43e6d0cf2e
ArfazAhamed <arfazahamed31@gmail.com>
2023-06-11 04:23:32 +0600

    check-admin

--- index.js
+++ index.js
@@ -86,6 +86,21 @@ app.get('/users',verifyJWT,  async(req, res) =>{
     })
 
 
+    
+    //check admin
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
 
 
 
