37d8a3c3e246c5b8e85c19dc6b2b8c9a7c0b98d3
ArfazAhamed <arfazahamed31@gmail.com>
2023-06-10 23:38:29 +0600

    instructor api

--- index.js
+++ index.js
@@ -61,19 +61,19 @@ app.get('/users', async(req, res) =>{
       res.send(result)
     })
 
-
-    //instructor api
-    app.patch('/users/instructor/:id', async(req, res) =>{
-      const id = req.params.id
-      const filter = {_id: new ObjectId(id)}
-      const updateDoc = {
-        $set: {
-          role: 'instructor'
-        },
-      }
-      const result = await usersCollection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
+ //instructor api
+ app.patch('/users/instructor/:id', async(req, res) =>{
+  const id = req.params.id
+  const filter = {_id: new ObjectId(id)}
+  const updateDoc = {
+    $set: {
+      role: 'instructor'
+    },
+  }
+  const result = await usersCollection.updateOne(filter, updateDoc)
+  res.send(result)
+})
+   
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
